205b9fea36e4ab12fe38a69f188172ac3db5cce9
ebipresh02 <ebipresh02@gmail.com>
2024-08-31 23:54:46 +0100

    initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+This script fetches the total number of subscribers for a given
+subreddit. If an invalid subreddit is given, the function 
+should return 0.
+
+Modules Used:
+    - JSON for parsing JSON data.
+    - Urllib for fetching data from a given url.
+
+"""
+
+import json
+import urllib
+
+
+def number_of_subscribers(subreddit):
+    # URL to fetch the data from
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    # A custom User-Agent to avoid too many requests error
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    # A request object with custom headers
+    request = urllib.request.Request(url, headers=headers)
+
+    with urllib.request.urlopen(request) as response:
+        if response.status == 200:
+            data = json.loads(response.read().decode()))
+            return data['data']['subscribers']
+        else:
+            return 0
+
