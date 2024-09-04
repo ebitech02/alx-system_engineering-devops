157082ef96ad297e4ac166545b0527a2fe3a5033
ebipresh02 <ebipresh02@gmail.com>
2024-09-04 21:34:20 +0100

    testing code -1

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,44 @@
 # fixing internal server error bug
 
-exec { 'apply-fix':
+package { 'apache2':
+  ensure  => installed,
+}
+
+file { '/var/www/html':
+  ensure => directory,
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0755',
+}
+
+file { '/var/www/html/directory':
+  ensure => directory,
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0755',
+}
+
+file { '/var/log/apache2':
+  ensure => directory,
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0755',
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
+}
+
+exec { 'correct-phpp-php':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/',
+}
+
+exec { 'restart-apache':
+  command     => 'service restart apache2',
+  path        => '/sbin:/usr/sbin:/bin',
+  refreshonly => true,
+  subscribe   => Exec['replace-apache'],
 }
 
