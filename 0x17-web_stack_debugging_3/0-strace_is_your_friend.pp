640c2746ff6fe06837f1fc068bbeaf6b5bd9f4ff
ebipresh02 <ebipresh02@gmail.com>
2024-09-04 17:54:11 +0100

    initial code

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,25 @@
+# Ensures Apache has the correct directory and permissions
+
+package { 'apache2':
+  ensure =>  installed,
+}
+
+file { '/var/www/html':
+  ensure =>  directory,
+  owner  =>  'www-data',
+  group  =>  'www-data',
+  mode   =>  '0755',
+}
+
+file { '/var/log/apache2':
+  ensure => directory,
+  owner  =>  'www-data',
+  group  =>  'www-data',
+  mode   =>  '0755',
+}
+
+service { 'apache2':
+  ensure =>  running,
+  enable => true,
+}
+
