95a005bf8c00f58385f0be01e0313a9efd834451
ebipresh02 <ebipresh02@gmail.com>
2024-09-04 21:06:22 +0100

    new code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -30,3 +30,9 @@ service { 'apache2':
   enable => true,
 }
 
+exec { 'apply-fix':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
+  unless  => 'grep -q "php" /var/www/html/wp-settings.php',
+}
+
