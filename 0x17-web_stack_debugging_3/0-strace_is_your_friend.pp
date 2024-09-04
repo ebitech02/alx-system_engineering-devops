0a35fc32b1d10dce3ae23e7ea74315c7fe2a81ca
ebipresh02 <ebipresh02@gmail.com>
2024-09-04 21:17:01 +0100

    testing

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 exec { 'apply-fix':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/',
-  unless  => 'grep -q "php" /var/www/html/wp-settings.php',
 }
 
