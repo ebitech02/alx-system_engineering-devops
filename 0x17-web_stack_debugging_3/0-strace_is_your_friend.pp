4f5af76bfcdc7bbe77f12ce1b767ac6c88614601
ebipresh02 <ebipresh02@gmail.com>
2024-09-04 21:58:26 +0100

    updatedd

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,8 @@
 # fixing internal server error bug
-
-package { 'apache2':
-  ensure  => installed,
-}
 # make sure the wp-settings.php has the correct extensions
 
 exec { 'correct-phpp-php':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/usr/local/bin', '/usr/bin'],
-  onlyif  => 'grep -q "phpp" /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
 }
 
