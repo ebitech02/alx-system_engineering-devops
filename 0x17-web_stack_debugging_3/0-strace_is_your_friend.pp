f8faeb0bcef44b708add6890700f60bd53554157
ebipresh02 <ebipresh02@gmail.com>
2024-09-04 21:54:53 +0100

    hope this works

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,35 +3,11 @@
 package { 'apache2':
   ensure  => installed,
 }
-
-file { '/var/www/html':
-  ensure => directory,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0755',
-}
-
-file { '/var/www/html/directory':
-  ensure => directory,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0755',
-}
-
-file { '/var/log/apache2':
-  ensure => directory,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0755',
-}
-
-service { 'apache2':
-  ensure => running,
-  enable => true,
-}
+# make sure the wp-settings.php has the correct extensions
 
 exec { 'correct-phpp-php':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => ['/usr/local/bin', '/usr/bin'],
+  onlyif  => 'grep -q "phpp" /var/www/html/wp-settings.php',
 }
 
